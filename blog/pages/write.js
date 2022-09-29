7b57a6c4c1f649042f095548dee9327d1ea7d635
glorangJohn <42515875+Insookim0702@users.noreply.github.com>
2022-09-29 10:05:29 +0900

    [feat] 글쓰기 페이지 추가

---
+++ blog/pages/write.js
@@ -0,0 +1,15 @@
+export default function write() {
+  return (
+    <>
+      <header>글쓰기</header>
+      <form>
+        <h1>제목</h1>
+        <input />
+        <h1>내용</h1>
+        <textarea />
+        <br />
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
