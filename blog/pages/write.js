c47998302f8052cd831e9a6691c281b900444d96
glorangJohn <42515875+Insookim0702@users.noreply.github.com>
2022-10-03 10:19:46 +0900

    [feat] 글 작성일 표시 추가 및  글쓰기 폼 서브밋 이벤트 실행 함수 작성

--- blog/pages/write.js
+++ blog/pages/write.js
@@ -1,14 +1,23 @@
+import { useRef } from 'react'
 export default function write() {
+  const title = useRef('')
+  const content = useRef('')
+  function onSubmit(event) {
+    event.preventDefault()
+    console.log(title.current.value)
+    console.log(content.current.value)
+  }
+
   return (
     <>
       <header>글쓰기</header>
       <form>
         <h1>제목</h1>
-        <input />
+        <input ref={title} />
         <h1>내용</h1>
-        <textarea />
+        <textarea ref={content} />
         <br />
-        <input type="submit" />
+        <input type="submit" onClick={onSubmit} />
       </form>
     </>
   )
